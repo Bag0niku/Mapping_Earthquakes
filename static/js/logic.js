d9a7563f537a0a6c2c5cd31318dac46a9e19e449
Bag0niku <nick@bag0niku.com>
2022-08-31 02:37:27 -0700

    decided on a marker style and have a couple of map layers

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,6 +9,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 console.log("Loading map")
 
+
+// streetview tile layer for the map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -17,7 +19,25 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     zoomOffset: -1,
     accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+
+// Darkview tile layer option for the map
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+// base layer holding both layer options
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+  };
+
+// add baseMap to control the layer options
+L.control.layers(baseMaps).addTo(map)
+
+let LAcircle = L.circleMarker([34.0522, -118.2437], {radius: 100, color: "#99ff66", fillcolor: "#99ff66" }).addTo(map);
+ 
 
 console.log("Map Loading Complete")
