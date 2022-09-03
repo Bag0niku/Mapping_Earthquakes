eb80589ab2a9a02294e144ebb47ddc82deae89d2
Bag0niku <nick@bag0niku.com>
2022-09-02 21:20:51 -0700

    customized tectonic plates color and line width, and added Topographical map layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -19,11 +19,13 @@ const plates_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/mast
 
 function createTPlates(layer, url) {
     d3.json(url).then((data) => {
+        let tPlates = L.geoJSON()
         Object(data.features).forEach(feature => {
             let latlng = feature.geometry.coordinates.map(d => d.reverse());
-            let plate = L.polyline(latlng, {color:"Black"});
-            plate.addTo(layer);    
+            let plate = L.polyline(latlng, {color:"#FF00FF", weight:3});
+            plate.addTo(tPlates);    
         });
+        tPlates.addTo(layer);
     });
 }
 
@@ -88,6 +90,13 @@ function createMap() {
         zoomOffset: -1,
         accessToken: API_KEY
     });
+    
+    const outdoor = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/outdoors-v11',
+            accessToken: API_KEY
+        });
 
     // Darkview tile layer option for the map
     const darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -107,7 +116,9 @@ function createMap() {
     const baseMaps = {
         Street: streetmap,
         Dark: darkmap,
-        Satellite: satelliteStreets
+        Satellite: satelliteStreets,
+        Topographical: outdoor
+
     };
 
     // Create the layer for the earthquakes
